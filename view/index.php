11cc794e8fe5190091d7118dfefbd74ffe3c42f7
schelpstech <sunkanmi@schelps.com.ng>
2022-09-16 18:49:11 +0100

    admission

---
+++ view/index.php
@@ -0,0 +1,10 @@
+<html>
+   <head>
+      <script type = "text/javascript">
+       
+            
+               window.location.replace("applicant");  
+         
+      </script>
+   </head>
+   </html>
