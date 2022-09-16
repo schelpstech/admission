229debe6f61e26816849d7bf6888fa74139641c5
schelpstech <sunkanmi@schelps.com.ng>
2022-09-16 21:44:35 +0100

    update

--- include/php/header.php
+++ include/php/header.php
@@ -9,7 +9,7 @@ include '../../controller/start.inc.php';
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Owlio - School Admission Admin Dashboard </title>
+    <title>Admission Portal </title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
 	<link href="vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
