3b783b45e9380b8eab1405605c4ad8d58fc112e8
schelpstech <sunkanmi@schelps.com.ng>
2022-09-16 22:32:20 +0100

    nav

--- include/php/navbar.php
+++ include/php/navbar.php
@@ -206,7 +206,7 @@ $count_form = $model->getRows($tblName, $conditions);
                             <span class="nav-text">Screening</span>
                         </a>
                     </li>
-                    <li><a class="ai-icon" href="javascript:void()" aria-expanded="false">
+                    <li><a class="ai-icon" href="../../view/applicant/formfiller.php?profile=<?php echo $_SESSION['uniqueid'] ?>" aria-expanded="false">
                             <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="32" height="32" viewBox="0 0 24 24" fill="grey" stroke="grey" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                                 <circle cx="12" cy="7" r="4"></circle>
